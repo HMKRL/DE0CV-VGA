d133dbc677b126f0b47e78f3576961aa0357f8ed
Yu-Cheng Liang <a135246a135246@gmail.com>
2017-01-04 01:48:02 +0800

    Finished requirement

--- VGA_Practice.v
+++ VGA_Practice.v
@@ -1,22 +1,69 @@
-module VGA_Practice(rst, clk, vga_red, vga_green, vga_blue, hsync, vsync, valid, select);
+module VGA_Practice (rst, clk, vga_red, vga_green, vga_blue, hsync, vsync, type);
+	input rst, clk;
+	output [3:0]vga_red;
+	output [3:0]vga_green;
+	output [3:0]vga_blue;
+	output hsync;
+	output vsync;
+
+	input [1:0]type;
+
+	VGA disp(rst, clk, vga_red, vga_green, vga_blue, hsync, vsync, type);
+	
+endmodule
+
+module VGA(rst, clk, vga_red, vga_green, vga_blue, hsync, vsync, type);
 	input clk;
 	input rst;
-	input [2:0]select;
+	input [1:0]type;
 	
 	output reg [3:0]vga_red;
 	output reg [3:0]vga_green;
 	output reg [3:0]vga_blue;
-	output hsync, vsync, valid;
-	
-	
+	output hsync, vsync;
+
+	wire [10:0]horizon;
+	wire [10:0]verticle;
+		
 	always @(posedge clk) begin
 		if(valid) begin
-			if(select[2]) vga_red = 4'b1111;
-			else vga_red = 0;
-			if(select[1]) vga_green = 4'b1111;
-			else vga_green = 0;
-			if(select[0]) vga_blue = 4'b1111;
-			else vga_blue = 0;
+			case (type)
+				2'b00: begin //black
+					vga_red = 0;
+					vga_green = 0;
+					vga_blue = 0;
+				end
+				2'b01: begin //red
+					if	(((horizon <= 200 || horizon >= 600) && (verticle <= 150 || verticle >= 450))  ||  ((horizon >= 200 && horizon <= 600 && verticle >= 150 && verticle <= 450))) begin
+						vga_red = 15;
+					end
+					else begin 
+						vga_red = 0;
+					end
+					vga_green = 0;
+					vga_blue = 0;
+				end
+				2'b10: begin //green
+					if	(((horizon <= 200 || horizon >= 600) && (verticle <= 150 || verticle >= 450))  ||  ((horizon >= 200 && horizon <= 600 && verticle >= 150 && verticle <= 450))) begin
+						vga_green = 0;
+					end
+					else begin 
+						vga_green = 15;
+					end
+					vga_red = 0;
+					vga_blue = 0;
+				end
+				2'b11: begin //blue
+					if(!((horizon >= 200 && horizon <= 600 && verticle >= 150 && verticle <= 450))) begin
+						vga_blue = 15;
+					end
+					else begin 
+						vga_blue = 0;
+					end
+					vga_green = 0;
+					vga_red = 0;
+				end
+			endcase
 		end
 		else begin
 			vga_red = 0;
@@ -25,11 +72,11 @@ module VGA_Practice(rst, clk, vga_red, vga_green, vga_blue, hsync, vsync, valid,
 		end
 	end
 	
-	synchrogazer VGA(clk, rst, hsync, vsync, valid);
+	synchrogazer VGA(clk, rst, hsync, vsync, valid, horizon, verticle);
 	
 endmodule
 
-module synchrogazer(clk, rst, hsync, vsync, valid);
+module synchrogazer(clk, rst, hsync, vsync, valid, horizon, verticle);
 
 	input clk;
 	input rst;
@@ -37,9 +84,8 @@ module synchrogazer(clk, rst, hsync, vsync, valid);
 	output reg hsync;
 	output reg vsync;
 	output reg valid;
-
-	reg [10:0]horizon;
-	reg [10:0]verticle;
+	output reg [10:0]horizon;
+	output reg [10:0]verticle;
 
 	always @(posedge clk) begin
 		if(!rst) begin
@@ -90,4 +136,8 @@ module synchrogazer(clk, rst, hsync, vsync, valid);
 	else 
 		valid <= (horizon < 800 && verticle < 600);  
 	end
+endmodule
+
+module picture ();
+	
 endmodule
